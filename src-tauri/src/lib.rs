913645aba5bc1e866f6049aec1d2fabd47640d35
Command1264 <command1264@gmail.com>
2025-05-24 06:01:40 +0800

    feat: 新增 post_action 功能，支援後台傳送資料給機器人

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,9 +1,36 @@
+use std::collections::HashMap;
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+use reqwest::Client;
+
+
 #[tauri::command]
 async fn greet() -> String {
     match reqwest::get("http://127.0.0.1:5000").await {
         Ok(resp) => resp.text().await.unwrap_or_else(|_| "讀取回應失敗".to_string()),
         Err(_) => "無法連線到伺服器".to_string(),
+    }
+}
+
+#[tauri::command]
+async fn post_action(
+    url: String,
+    angle: f64,
+    direction: String,
+    force: f64
+) -> bool {
+    let client = Client::new();
+    let mut map = HashMap::new();
+    map.insert("direction", direction);
+    match client.post(url)
+        .json(&map)
+        .send()
+        .await {
+        Ok(resp) => {
+            resp.status().is_success()
+        }
+        Err(_) => {
+            false
+        }
     }
 }
 
