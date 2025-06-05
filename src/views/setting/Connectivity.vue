e2989824003aed4511462383aee453a1f7feae84
Command1264 <command1264@gmail.com>
2025-06-06 01:41:36 +0800

    refactor(ui): 將 Connectivity.vue 中的 networkConfig 更名為 config - 將變數名稱 networkConfig 改為 config，簡化命名 - 提升可讀性與與其他模組命名一致性 - 未影響功能邏輯與資料流

--- src/views/setting/Connectivity.vue
+++ src/views/setting/Connectivity.vue
@@ -3,7 +3,7 @@ import { onMounted, onUnmounted, reactive, watch } from "vue";
 import { setting_api, Settings, webSocket_api } from "@/assets/ts/tauri_api.ts";
 import lodash from "lodash";
 /** 網路連線設定 */
-const networkConfig = reactive<Settings>({
+const config = reactive<Settings>({
   control: {
     joystick_sensitivity: 0.2,
     joystick_size: 150,
@@ -15,17 +15,17 @@ const networkConfig = reactive<Settings>({
     port: 8787,
   },
 });
-const networkConfigConnectOrigin = {};
+const configConnectOrigin = {};
 // 載入設定
 const loadSettings = async () => {
   const result = await setting_api.getSetting();
-  Object.assign(networkConfigConnectOrigin, result.connect);
-  Object.assign(networkConfig, result);
+  Object.assign(configConnectOrigin, result.connect);
+  Object.assign(config, result);
 };
 
 // 即時儲存設定（防抖可加）
 const saveSettings = async () => {
-  await setting_api.setSetting(networkConfig);
+  await setting_api.setSetting(config);
 };
 
 // 初始化時載入設定
@@ -34,14 +34,14 @@ onMounted(() => {
 });
 // 離開網路連線頁面刷新webSocket
 onUnmounted(() => {
-  if (!lodash.isEqual(networkConfigConnectOrigin, networkConfig.connect)) {
+  if (!lodash.isEqual(configConnectOrigin, config.connect)) {
     webSocket_api.reconnectWs();
   }
 });
 
 // 監聽 networkConfig，深層 watch 自動儲存
 watch(
-  () => networkConfig,
+  () => config,
   () => {
     saveSettings();
   },
@@ -53,12 +53,12 @@ watch(
     <h1 class="setting-title">網路連接 Connectivity</h1>
     <van-cell-group class="setting-content-group" inset>
       <van-field
-        v-model="networkConfig.connect.url"
+        v-model="config.connect.url"
         label="連線網址"
         class=".setting-cell"
       />
       <van-field
-        v-model.number="networkConfig.connect.port"
+        v-model.number="config.connect.port"
         type="digit"
         label="連線 port"
       />
